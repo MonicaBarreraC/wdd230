857e823095564326d2c918ca6b24471bbd1cd81a
MonicaBarreraC <u653058995@byui.edu>
2022-02-01 16:02:57 -0600

    Date Fixed

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -5,3 +5,18 @@ function toggleMenu(){
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+/*    D A T E    */
+
+// select the elements to manipulate (output to)
+const datefieldUK = document.querySelector("#currentDate"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options -> more options
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
