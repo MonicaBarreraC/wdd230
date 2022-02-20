c6c07e70aab310152d8951bd7765ec2e7edcc565
MonicaBarreraC <u653058995@byui.edu>
2022-02-19 21:04:53 -0600

    Windchill File

---
+++ chamber/js/windchill.js
@@ -0,0 +1,12 @@
+// Windchill Calculation
+
+/* SPECS
+get the two input values of temperature and wind speed,
+check to make sure they meet the specification limits  
+(<=50°F and >3.0mph) allowed to officially calculate the wind chill, and
+either calculate and display the wind chill factor value 
+or display "N/A" (not applicable) if the input values did not meet the requirements.
+*/
+
+let temperature = document.getElementById("temperature")
+console.log(temperature)
