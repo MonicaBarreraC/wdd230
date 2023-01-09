1a50296968d64232a4020ee4ef345fcae9e0f255
MonicaBarreraC <u653058995@byui.edu>
2023-01-09 11:27:23 -0600

    Last Update

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,4 @@ const year = new Date().getFullYear();
 document.querySelector("#year").textContent = year;
 
 // Last Update
-let lastUpdate = document.lastModified;
-document.querySelector("#last-update").textContent = `Last Updated: ${lastUpdate}`
+document.querySelector("#last-update").textContent = `Last Updated: ${document.lastModified}`
