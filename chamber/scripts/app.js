9b0779b958e4877f4d09bd27359af4f358db4b7b
MonicaBarreraC <u653058995@byui.edu>
2023-01-25 17:42:08 -0600

    Current Date

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,3 +1,7 @@
+// Current Date
+//document.querySelector("#current-date").textContent = `${new Date().toLocaleDateString('en-uk', {weekday: "long"})}`;
+document.querySelector("#current-date").textContent = `${new Date().toLocaleDateString('en-uk', {dateStyle: "full"})}`;
+
 // Get Year
 const year = new Date().getFullYear();
 document.querySelector("#copyright-year").textContent = `© ${year}`;
