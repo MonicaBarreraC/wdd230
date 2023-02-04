48cef5a93f8a745667b9eb88013492f4fa657b73
MonicaBarreraC <u653058995@byui.edu>
2023-02-04 14:32:56 -0600

    Button Functions Added

--- lesson5/scripts/app.js
+++ lesson5/scripts/app.js
@@ -1,16 +1,33 @@
 //
-const inp = document.querySelector('input');
-const addBtn = document.querySelector('button');
-const chapList = document.getElementById('#list');
+const inp = document.querySelector("input");
+const addBtn = document.querySelector("button");
+const chapList = document.getElementById("list");
 
 addBtn.addEventListener("click", function(){
 
     if (inp.value != ""){
 
-        alert("Hi!");
-        console.log(inp.value);
+        //alert("Hi!");
+        //console.log(inp.value);
+        
+        const liElem = document.createElement("li");
+        const delBtn = document.createElement("button");
 
+        liElem.textContent = inp.value;
+        delBtn.textContent = "❌";
 
+        chapList.appendChild(liElem);
+        liElem.appendChild(delBtn);
+
+        delBtn.addEventListener("click", function(){
+            liElem.remove();
+            document.getElementById("favchap").focus();
+        });
     }
+
+    inp.value = "";
+    document.getElementById("favchap").focus();
     
 });
+
+document.getElementById("favchap").focus();
