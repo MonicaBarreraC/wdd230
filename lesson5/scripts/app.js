36001beb4b6973aaf99894253c0865f6b9b33659
MonicaBarreraC <u653058995@byui.edu>
2023-02-04 14:08:26 -0600

    Script Basics

---
+++ lesson5/scripts/app.js
@@ -0,0 +1,16 @@
+//
+const inp = document.querySelector('input');
+const addBtn = document.querySelector('button');
+const chapList = document.getElementById('#list');
+
+addBtn.addEventListener("click", function(){
+
+    if (inp.value != ""){
+
+        alert("Hi!");
+        console.log(inp.value);
+
+
+    }
+    
+});
