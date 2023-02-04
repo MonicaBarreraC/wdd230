708ed1debcabff4ebef75897aa7e9d1ce1bed239
MonicaBarreraC <u653058995@byui.edu>
2023-02-04 14:36:13 -0600

    Comments Added to Code

--- lesson5/scripts/app.js
+++ lesson5/scripts/app.js
@@ -1,33 +1,37 @@
-//
+// Select Elements
 const inp = document.querySelector("input");
 const addBtn = document.querySelector("button");
 const chapList = document.getElementById("list");
 
+// Submit Button
 addBtn.addEventListener("click", function(){
 
     if (inp.value != ""){
-
-        //alert("Hi!");
-        //console.log(inp.value);
         
+        // Create list and button elements
         const liElem = document.createElement("li");
         const delBtn = document.createElement("button");
 
+        // Set content
         liElem.textContent = inp.value;
         delBtn.textContent = "❌";
 
+        // Append elements to list (ul)
         chapList.appendChild(liElem);
         liElem.appendChild(delBtn);
 
+        // Delete Button
         delBtn.addEventListener("click", function(){
             liElem.remove();
             document.getElementById("favchap").focus();
         });
     }
 
+    // Clear and Focus Input
     inp.value = "";
     document.getElementById("favchap").focus();
     
 });
 
+// Focus Input
 document.getElementById("favchap").focus();
