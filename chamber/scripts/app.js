c130f518e94f499768c43ef0aba5b90d67c09cf2
MonicaBarreraC <u653058995@byui.edu>
2023-02-04 20:14:30 -0600

    Script for Banner News

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,12 +1,27 @@
 // Current Date
 document.querySelector("#current-date").textContent = `${new Date().toLocaleDateString('en-uk', {dateStyle: "full"})}`;
 
+// Get Day
+const day = new Date().getDay();
+/// Mondays and Tuesdays
+if (day == 1 || day == 2 || day == 6){
+    console.log(day);
+    document.querySelector("#banner-news").removeAttribute("class");
+}
+/// Rest of the days
+else {
+    console.log("Nope");
+    document.querySelector("#banner-news").setAttribute("class", "hidden");
+}
+
 // Get Year
 const year = new Date().getFullYear();
 document.querySelector("#copyright-year").textContent = `© ${year}`;
 
 // Last Update
 document.querySelector("#last-update").textContent = `Last Updated: ${document.lastModified}`
+
+// -  -  -  -  -  -  -  -  -  -  
 
 // Hamburger Menu
 const mainNav = document.querySelector('.navigation');
