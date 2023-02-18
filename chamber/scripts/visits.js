b9fdea7345eba3c0706a0a200fd823a2e462e4f9
MonicaBarreraC <u653058995@byui.edu>
2023-02-17 19:50:03 -0600

    Days to Festival JavaScript

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -2,11 +2,11 @@
 
 // Not needed actually because I can just create a new variable and don't grab info from the HTML file
 let visitMessage = document.querySelector("#n-visits").textContent;
-console.log(visitMessage);
+//console.log(visitMessage);
 
 // Get number of visits from localStorage
 let nVisits = Number(window.localStorage.getItem("visits-ls")); /* Number() prevents "Undefined" or something like that */
-console.log(nVisits);
+//console.log(nVisits);
 
 // First visit message
 if (nVisits == 0) {
@@ -25,14 +25,38 @@ nVisits ++;
 // Save changes in localStorage
 localStorage.setItem("visits-ls", nVisits);
 
-// -  -  -  -  
+// -  -  -  -  F E S T I V A L S  -  -  -  -
 
 // Today
 const today = new Date();
-console.log(today);
+console.log("DAY: " + today.getDate()); /* getDate (1-31), getDay (0-6) */
+console.log("MONTH: " + today.getMonth()); /* From 0 to 11 */
+console.log("YEAR: " + today.getFullYear());
 
-// 5 mayo
+// May 5 -> Virgen de la concepción
+const virgen = new Date(today.getFullYear(), 4, 5); /* Subtract 1 to the month (0-11) */
 
-// 1 octubre
+// Avoid negative numbers changing to next year if the date has passed
+if (today.getMonth() == 4 && today.getDate() > 5) {
+    virgen.setFullYear(virgen.getFullYear() + 1);
+}
+// Days Left to May 5
+let daysLeftVirgen = (virgen.getTime() - Date.now()) / 84600000; /* 84600000 */
+/// 0 Decimals
+daysLeftVirgen = daysLeftVirgen.toFixed(0);
+
+// Update HTML file
+// 0 days to festival
+if (daysLeftVirgen == 0) {
+    document.querySelector("#virgen-festival").textContent = `today.`;
+} else if (daysLeftVirgen == 1) {
+    document.querySelector("#virgen-festival").textContent = `tomorrow.`;
+} else {
+    document.querySelector("#virgen-festival").textContent = `in ${daysLeftVirgen} days.`;
+}
+
+
+// October 1 -> St. Francisco de Asís
+const francisco = new Date(today.getFullYear(), 9, 1);
 
 
