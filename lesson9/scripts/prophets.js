10fbee6e41d274d81c1d7d5acfd87f29235e59e3
MonicaBarreraC <u653058995@byui.edu>
2023-02-28 21:40:00 -0600

    Prophets Initial HTML Structure

---
+++ lesson9/scripts/prophets.js
@@ -0,0 +1,9 @@
+// Get Year
+const year = new Date().getFullYear();
+document.querySelector("#copyright-year").textContent = `© ${year}`;
+
+// Last Update
+document.querySelector("#last-update").textContent = `Last Updated: ${document.lastModified}`
+
+// -  -  -  -  P R O P H E T S  -  -  -  -  -
+
