24b96047e1029151b09b52feb2b18f5a69e8fa87
MonicaBarreraC <u653058995@byui.edu>
2023-03-07 23:02:46 -0600

    Geting Data from API Correct

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -12,7 +12,25 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?q={city name}&appid=4c0f9bbc39fef5d82166c66bab59f1ed';
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=4c0f9bbc39fef5d82166c66bab59f1ed';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
 
 //http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
 
+//https://api.openweathermap.org/data/2.5/weather?q=Conkal&appid=4c0f9bbc39fef5d82166c66bab59f1ed`;
