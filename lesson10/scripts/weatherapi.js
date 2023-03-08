5ee5cd14cfb69dda487810daa5bc9d9de691dcd4
MonicaBarreraC <u653058995@byui.edu>
2023-03-07 23:26:04 -0600

    Weather APi to fahrenheit

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -19,8 +19,8 @@ async function apiFetch() {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
+        //console.log(data); // this is for testing the call
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
@@ -31,6 +31,22 @@ async function apiFetch() {
   
   apiFetch();
 
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
+function displayResults(weatherData) {
+    // Kelvin to Fahrenheit
+    const degrees = kelvinToFahrenheit(weatherData.main.temp);
 
-//https://api.openweathermap.org/data/2.5/weather?q=Conkal&appid=4c0f9bbc39fef5d82166c66bab59f1ed`;
+    currentTemp.innerHTML = `<strong>${degrees}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
+
+function kelvinToFahrenheit(kelvin) {
+    //(°K − 273.15) × 9/5 + 32 = °F
+    const fahrenheit = ((kelvin) - 273.15) * (9/5) + 32;
+    return fahrenheit.toFixed(0);
+}
