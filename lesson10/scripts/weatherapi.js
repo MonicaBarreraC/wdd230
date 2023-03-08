23d5bfc14086161ced7791cbf253f91fc7f32cf6
MonicaBarreraC <u653058995@byui.edu>
2023-03-08 17:12:03 -0600

    Test Celsius

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -12,7 +12,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=4c0f9bbc39fef5d82166c66bab59f1ed';
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=4c0f9bbc39fef5d82166c66bab59f1ed&units=metric';
 
 async function apiFetch() {
     try {
@@ -33,9 +33,9 @@ async function apiFetch() {
 
 function displayResults(weatherData) {
     // Kelvin to Fahrenheit
-    const degrees = kelvinToFahrenheit(weatherData.main.temp);
+    //const degrees = kelvinToFahrenheit(weatherData.main.temp);
 
-    currentTemp.innerHTML = `<strong>${degrees}</strong>`;
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
@@ -44,9 +44,9 @@ function displayResults(weatherData) {
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
 }
-
+/*
 function kelvinToFahrenheit(kelvin) {
     //(°K − 273.15) × 9/5 + 32 = °F
     const fahrenheit = ((kelvin) - 273.15) * (9/5) + 32;
     return fahrenheit.toFixed(0);
-}
+}*/
