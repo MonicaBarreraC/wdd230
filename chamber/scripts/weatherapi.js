99bef78c323862aaf7f258b6b151f38d88a771a1
MonicaBarreraC <u653058995@byui.edu>
2023-03-11 10:34:41 -0600

    Weather JS Data Completed

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -1,18 +1,22 @@
 // -  -  -  -  W E A T H E R  -  -  -  -  -
 
 // select HTML elements in the document
-const currentTemp = document.querySelector('#weather-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('#weather-desc');
+const currentTemp = document.querySelector("#weather-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("#weather-desc");
+const degreesUnits = document.querySelector("#degrees");
+const windSpeed = document.querySelector("#wind-speed");
+const windsSpeedUnits = document.querySelector("#speed-units");
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=Conkal&appid=4c0f9bbc39fef5d82166c66bab59f1ed&units=imperial';
+
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Conkal&appid=4c0f9bbc39fef5d82166c66bab59f1ed&units=imperial";
 
 async function apiFetch() {
     try {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        //console.log(data); // this is for testing the call
+        console.log(data); // this is for testing the call
         displayResults(data);
       } else {
           throw Error(await response.text());
@@ -26,12 +30,15 @@ async function apiFetch() {
 
 function displayResults(weatherData) {
 
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
 
     const iconSrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
 
-    weatherIcon.setAttribute('src', iconSrc);
-    weatherIcon.setAttribute('alt', desc);
+    weatherIcon.setAttribute("src", iconSrc);
+    weatherIcon.setAttribute("alt", desc);
     captionDesc.textContent = desc;
+    degreesUnits.textContent = "F";
+    windSpeed.textContent = weatherData.wind.speed.toFixed(1);
+    windsSpeedUnits.textContent = "mph";
 }
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,87 +1,118 @@
-// -  -  -  -  -  B U T T O N  -  -  -  -  -  -
-
-const degreesButton = document.querySelector("#degree-button");
-
-degreesButton.addEventListener("click", function(){
-    // Change C <-> F
-    if ( degreesButton.textContent == "°C"){
-        degreesButton.textContent = "°F";
-        // Change Degrees Displayed
-        // Change Wind Speed Displayed
-    }
-    else {
-        degreesButton.textContent = "°C";
-        // Change Degrees Displayed
-        // Change Wind Speed Displayed
-    }
-});
-
 // -  -  -  -  C H A N G E  U N I T S  -  -  -  -  -
 
 // Formula: (0°C * 9/5) + 32 = 32°F
 function celsiusToF(temperature) {
-    temperature = (temperature * 9/5) + 32;
+    temperature = (temperature * 9 / 5) + 32;
     return temperature;
 }
 
 // Formula: (32°F − 32) × 5/9 = 0°C
 function fahrenheitToC(temperature) {
-    temperature = (temperature - 32) * 5/9;
+    temperature = (temperature - 32) * 5 / 9;
     return temperature;
 }
 
+// Formula: Divide speed by 1.609344
+function kmToMph(speed) {
+    speed = speed / 1.609344;
+    return speed;
+}
+
+// Formula: Divide speed by 1.609344
+function mphToKmh(speed) {
+    speed = speed * 1.609344;
+    return speed;
+}
 
 // -  -  -  -  W I N D  C H I L L   -  -  -  -  -
+
+function calcWindChill(temperature, windSpeed) {
+    let windChill;
+    // Temperature in Fahrenheit
+    // Check if they meet specifiaction limits (<=50°F and >3.0mph)
+    if (temperature <= 50 && windSpeed > 3.0){
+        windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+        windChill = Math.round(f * 10)/10;
+    }
+
+    // Not Applicable
+    else {
+        windChill = "N/A";
+    }
+
+    // Return Wind Chill
+    return windChill;
+}
 
 // Get values
 let t = document.querySelector("#weather-temp").textContent;
 let s = document.querySelector("#wind-speed").textContent;
-let f = document.querySelector("#wind-chill").textContent;;
+let f = document.querySelector("#wind-chill").textContent;
 let degrees = document.querySelector("#degrees").textContent;
 let speedUnits = document.querySelector("#speed-units").textContent;
 
+// Units Check
 // Convert °C to °F
 if (degrees == "C") {
-
     t = celsiusToF(t);
     degrees = "F";
-
 }
 
 // Convert km/h to mph
 // Formula: Divide speed by 1.609344
 if (speedUnits == "km/h") {
-
-    s = s / 1.609344;
+    s = kmToMph(s);
     speedUnits = "mph";
-
 }
 
 // Wind Chill Calculation
+f = calcWindChill(t, s);
 
-// Check if they meet spacifiaction limits
-// <=50°F and >3.0mph
-if (t <= 50 && s > 3.0){
-    f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
-    f = Math.round(f * 10)/10;
-}
-
-// Not Applicable
-else {
-    f = "N/A";
-}
-
-// Set Values
+// Set wind chill value
 document.querySelector("#wind-chill").textContent = f;
 
-// Change to °F and mph -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
-/*// Note: Uncomment this and add a button later to change C -> F and reverse
-t = Math.round(t);
-document.querySelector("#weather-temp").textContent = t;
-s = Math.round(s * 10)/10;
-document.querySelector("#wind-speed").textContent = s;
-document.querySelector("#degrees").textContent = degrees;
-document.querySelector("#speed-units").textContent = speedUnits;
-// -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  - */
+// -  -  -  -  -  B U T T O N  -  -  -  -  -  -
 
+const degreesButton = document.querySelector("#degree-button");
 
+degreesButton.addEventListener("click", function(){
+
+    let temp = document.querySelector("#weather-temp");
+    let wSpeed = document.querySelector("#wind-speed");
+    let degreesU = document.querySelector("#degrees");
+    let speedU = document.querySelector("#speed-units");
+
+    // Change C <-> F
+    if ( degreesButton.textContent == "°C"){
+        degreesButton.textContent = "°F";
+
+        // Change Degrees Displayed
+        t = fahrenheitToC(temp.textContent).toFixed(0);
+        degrees = "C";
+
+        // Change Wind Speed Displayed
+        s = mphToKmh(wSpeed.textContent).toFixed(1);
+        speedUnits = "kmh";
+
+        //console.log(`Fahrenheit to C\n${temp.textContent}${degreesU.textContent} -> ${t}${degrees}`);
+    }
+    else {
+        degreesButton.textContent = "°C";
+
+        // Change Degrees Displayed
+        t = celsiusToF(t).toFixed(0);
+        degrees = "F";
+
+        // Change Wind Speed Displayed
+        s = kmToMph(s).toFixed(1);
+        speedUnits = "mph";
+
+        //console.log(`Celsius to F\n${temp.textContent}${degreesU.textContent} -> ${t}${degrees}`);
+    }
+
+    temp.textContent = t;
+    degreesU.textContent = degrees;
+    wSpeed.textContent = s;
+    speedU.textContent = speedUnits;
+
+});
