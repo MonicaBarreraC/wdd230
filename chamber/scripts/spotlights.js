02605d73f0c17560177b00206930d55677d0afd2
MonicaBarreraC <u653058995@byui.edu>
2023-03-15 17:57:11 -0600

    Fixing Dupplicated Spotlights

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -1,39 +1,54 @@
 // Set url 
 const directoryUrl = "https://monicabarrerac.github.io/wdd230/chamber/json/data.json";
-let business = [];
+let premiumB = [];
 
 // Function to fetch data using async/await
 async function getBusinessData() {
     const response = await fetch(directoryUrl);
     const data = await response.json();
+    //console.table(data.companies);
 
     // Check if Membership -> Silver or Gold
-    membershipCheck(data.companies);
+    premiumB = membershipCheck(data.companies);
+    //console.table(premiumB);
 
     // Display Spotlights
-    displaySpotlights(business);
+    displaySpotlights(premiumB);
 }
 
 const membershipCheck = (companies) => {
+    let list = [];
     companies.forEach((company) => {
         if(company.membership == "Silver" || company.membership == "Gold"){
-            business.push(company);
+            list.push(company);
         }
     });
+
+    return list;
 }
 
 const displaySpotlights = (business) => {
     const spotlights = document.querySelector("section.spotlights"); 
+    let originalList = business;
     let selected = [];
+
+    //console.log("Original List:");
+    //console.table(originalList);
 
     for (let i = 0; i < 3; i++ ){
 
         // Select random business
-        let random = Math.floor(Math.random() * business.length);
-        selected.push(business[random]);
-        business.pop(random);
+        let random = Math.floor(Math.random() * originalList.length);
+        selected.push(originalList[random]);
+        originalList.splice(random, 1);
+        //console.log("Delete " + random);
+        //console.log("Original List Now:");
+        //console.table(originalList);
 
     }
+
+    //console.log("Final Selected:");
+    //console.table(selected);
   
     selected.forEach((company) => {
         // Create elements to add to the section.spotlights element
